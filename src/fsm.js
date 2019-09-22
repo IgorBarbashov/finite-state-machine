225c908daba5ad9a885b79948335fe558e58f670
Igor Barbashov <bis200705@gmail.com>
2019-09-23 01:04:49 +0300

    finite-state-machine/feat: add FSM class implementation

--- src/fsm.js
+++ src/fsm.js
@@ -1,61 +1,93 @@
+class State {
+    constructor(stateName, comesFrom) {
+        this.stateName = stateName;
+        this.comesFrom = comesFrom;
+        this.goesTo = null;
+    }
+    
+    goAway(goesTo) {
+        this.goesTo = goesTo;
+    }
+}
+
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) {
+            throw new Error('Не задана начальная конфигурация');
+        }
+        this.config = config;
+        this.allStates = Object.keys(config.states);
+        this.currentStates = [new State(this.config.initial, null)];
+        this.currentIndex = 0;
+    }
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    getState() {
+        return this.currentStates[this.currentIndex].stateName;
+    }
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    changeState(state) {
+        if (!this.allStates.includes(state)) {
+            throw new Error(`Состояния '${state}' нет в словаре FSM.`);
+        }
+        const newState = new State(state, this.currentStates[this.currentIndex]);
+        this.currentStates[this.currentIndex].goAway(newState);
+        this.currentStates.push(newState);
+        this.currentIndex += 1;
+    }
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
+    trigger(event) {
+        const currentState = this.currentStates[this.currentIndex];
+        const currentStateName = currentState.stateName;
+        const currentStateConfig = this.config.states[currentStateName].transitions;
+        const currentStateDictionary = Object.keys(currentStateConfig);
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+        if (!currentStateDictionary.includes(event)) {
+            throw new Error(`У состояния '${currentStateName}' нет правила перехода '${event}'.`);
+        }
+        const newStateName = currentStateConfig[event];
+        const newState = new State(newStateName, currentState);
+        currentState.goAway(newState);
+        this.currentStates.push(newState);
+        this.currentIndex += 1;
+    }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    reset() {
+        this.currentStates = [new State(this.config.initial, null)];
+        this.currentIndex = 0;
+    }
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+    getStates(event) {
+        if (!event) {
+            return this.allStates;
+        }
+        return this.allStates.filter(el => {
+            const dict = Object.keys(this.config.states[el].transitions);
+            return dict.includes(event);
+        });
+    }
 
-    /**
-     * Goes redo to state.
-     * Returns false if redo is not available.
-     * @returns {Boolean}
-     */
-    redo() {}
+    undo() {
+        const currentState = this.currentStates[this.currentIndex];
+        if (!currentState.comesFrom) {
+            return false;
+        }
+        this.currentIndex -= 1;
+        return true;
+    }
 
-    /**
-     * Clears transition history
-     */
-    clearHistory() {}
+    redo() {
+        if (!this.currentStates[this.currentIndex].goesTo) {
+            return false;
+        }
+        this.currentIndex += 1;
+        return true;
+    }
+
+    clearHistory() {
+        const currentStateName = this.currentStates[this.currentIndex].stateName;
+        this.currentStates = [new State(currentStateName, null)];
+        this.currentIndex = 0;
+    }
 }
 
 module.exports = FSM;
-
-/** @Created by Uladzimir Halushka **/
